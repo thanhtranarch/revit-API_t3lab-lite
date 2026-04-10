1630418ab43f53fdcd8e1b2ff7c7319755c7d65e
Tran Tien Thanh <134719719+thanhtranarch@users.noreply.github.com>
2026-04-10 13:11:40 +0700

    update tools

---
+++ T3Lab_Lite.extension/T3Lab_Lite.tab/Annotation.panel/smartalign.stack/Distribute.pulldown/Vertical.pushbutton/script.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) 2014-2016 Gui Talarico
+
+Smart Align
+Smart Align is part of PyRevit Plus Optional Extensions for PyRevit
+github.com/gtalarico | @gtalarico
+
+--------------------------------------------------------
+pyRevit Notice:
+pyRevit: repository at https://github.com/eirannejad/pyRevit
+
+"""
+
+__author__ = 'Gui Talarico | @gtalarico'
+__version__ = '0.4.0'
+__doc__ = 'Distribute Elements Vertically'
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+from smartalign.distribute import main
+from smartalign.core import Alignment, VERBOSE
+
+# ALIGN = Alignment.HDIST
+ALIGN = Alignment.VDIST
+main(ALIGN)
